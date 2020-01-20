9d365951082b4e2f687eb69538720349bb63f777
Pedro Fernandes <ferped@gmail.com>
2020-01-20 16:10:36 +0000

    Movement
    Left/Right Movement
    Simple Jump

---
+++ Paradise Parasite/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject Player;
+
+
+    private Vector3 Offset;
+
+    void Start()
+    {
+
+        Offset = transform.position - Player.transform.position;
+    }
+
+
+    void LateUpdate()
+    {
+
+        transform.position = Player.transform.position + Offset;
+    }
+}
---
+++ Paradise Parasite/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(x, 0) * speed;
+    }
+}
